fb678ffbd6b77f04e87bad277faec60137a0061d
sskibinss <skibin991@gmail.com>
2023-01-14 21:22:21 +0100

    Created Initial Backend of the Odin Project

---
+++ src/main/java/com/odin4nature/odinstore/OdinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.odin4nature.odinstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdinProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdinProjectApplication.class, args);
+    }
+
+}
