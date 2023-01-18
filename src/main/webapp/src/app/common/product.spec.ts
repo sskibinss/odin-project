cce4cf00c27ebd575884db9baec3da7103783c68
sskibinss <skibin991@gmail.com>
2023-01-19 00:00:39 +0100

    Create basic webapp. Add display of Products in the table.
    Generated basic Angular web application and added custom components to display the Products using created service that calling the backend. Added Cross-Origin support.

---
+++ src/main/webapp/src/app/common/product.spec.ts
@@ -0,0 +1,7 @@
+import { Product } from './product';
+
+describe('Product', () => {
+  it('should create an instance', () => {
+    expect(new Product("sku", "name", "desc", "100.00", "imageUrl", true, 30, Date.prototype, Date.prototype)).toBeTruthy();
+  });
+});
